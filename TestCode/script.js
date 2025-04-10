7a3880d0d9f160065ac42c55843c3e1bea89aeef
JeromeUriah <jeromeuriahjiajun.ng@student.uq.edu.au>
2025-04-10 21:55:32 +1000

    Additional Changes

--- TestCode/script.js
+++ TestCode/script.js
@@ -1,13 +1,21 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
     loop: true,
-    loopFillGroupWithBlank: true,
+
+    // If we need pagination
     pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    el: '.swiper-pagination',
     },
+
+    // Navigation arrows
     navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+    el: '.swiper-scrollbar',
     },
 });
